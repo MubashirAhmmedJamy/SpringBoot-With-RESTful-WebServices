b2fc80e7bfc13643390c96776cb2c374bde257ee
Jamy <mubashirjamy@gmail.com>
2021-02-18 18:29:44 +0600

    Learning Spring Boot rest Controllers

---
+++ src/main/java/SpringBootRestController/BooksController.java
@@ -0,0 +1,18 @@
+package SpringBootRestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+	
+	@GetMapping("/books")
+	public List<Book> allBooks(){
+		
+		return Arrays.asList(new Book(123,"Mastering Spring Boot", "Jamy"));
+		
+	}
+}
---
+++ src/main/java/SpringBootRestController/SpringBootRestControllerApplication.java
@@ -0,0 +1,12 @@
+package SpringBootRestController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestControllerApplication.class, args);
+	}
+}
