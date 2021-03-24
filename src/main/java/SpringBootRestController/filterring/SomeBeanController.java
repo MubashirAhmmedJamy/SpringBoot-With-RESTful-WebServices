3c69bd21c817719ad78c9524470c58faa6eac6c9
Jamy <mubashirjamy@gmail.com>
2021-03-24 20:55:36 +0600

    Removed Code redundancy from filtering controllers

--- src/main/java/SpringBootRestController/filterring/SomeBeanController.java
+++ src/main/java/SpringBootRestController/filterring/SomeBeanController.java
@@ -2,7 +2,6 @@ package SpringBootRestController.filterring;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,57 +12,76 @@ import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
-
 @RestController
 public class SomeBeanController {
+
 	
-	@RequestMapping(method=RequestMethod.GET, path="/filterring")
-	public SomeBean retrieveSomeBean(){
+	//Static Filtering Methods
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/filterring")
+	public SomeBean retrieveSomeBean() {
 		System.out.println("Filterring Working");
 		return new SomeBean("Value1", "Value2", "Value3");
 	}
-	
-	@RequestMapping(method=RequestMethod.GET, path="/filterring-list")
-	public List<SomeBean> retrieveListOfSomeBean(){
+
+	@RequestMapping(method = RequestMethod.GET, path = "/filterring-list")
+	public List<SomeBean> retrieveSomeBeanList() {
 		return Arrays.asList(new SomeBean("Value1", "Value2", "Value3"), new SomeBean("Value12", "Value22", "Value32"));
 	}
+
 	
-	@RequestMapping(method=RequestMethod.GET, path="/filterring-dynamic")
-	public MappingJacksonValue retrieveSomeBeanDynamic(){
-		
-		System.out.println("DynamicFilterring Working");
-		
+	
+	//Dynamic Filtering Methods
+	@RequestMapping(method = RequestMethod.GET, path = "/filterring-dynamic")
+	public MappingJacksonValue retrieveSomeBeanDynamic() {
+		System.out.println("DynamicFilterring for bean Working");
+
 		SomeBean someBean = new SomeBean("Value1", "Value2", "Value3");
-		
-		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1","field3");
+		String properties[] = { "field1", "field2" };
+
+		return getMapping(someBean, properties);
+	}
+	
+
+	@RequestMapping(method = RequestMethod.GET, path = "/filterring-dynamic-list")
+	public MappingJacksonValue retrieveSomeBeanDynamicList() {
+		System.out.println("Dynamic Filterring for list working");
+
+		List<SomeBean> list = Arrays.asList(new SomeBean("Value1", "Value2", "Value3"),
+				new SomeBean("Value12", "Value22", "Value32"));
+		String properties[] = { "field2" };
+
+		return getMapping(list, properties);
+	}
+
+	private MappingJacksonValue getMapping(Object object, String[] properties) {
+
+		System.out.println("Getting Mappring for: " + object.toString() + " | " + properties.getClass());
+
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept(properties);
 		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBean Filter", filter);
-		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+		MappingJacksonValue mapping = new MappingJacksonValue(object);
 		mapping.setFilters(filters);
 		return mapping;
 	}
+
+	// private MappingJacksonValue getMapping(Object object, Set<String>
+	// properties){
+	// SimpleBeanPropertyFilter filter =
+	// SimpleBeanPropertyFilter.filterOutAllExcept(properties);
+	// FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBean
+	// Filter", filter);
+	// MappingJacksonValue mapping = new MappingJacksonValue(object);
+	// mapping.setFilters(filters);
+	// return mapping;
+	// }
 	
 	
-	@RequestMapping(method=RequestMethod.GET, path="/filterring-dynamic-list")
-	public MappingJacksonValue retrieveListOfSomeBeanDynamic(){
-		
-		List<SomeBean> list = Arrays.asList(new SomeBean("Value1", "Value2", "Value3"), new SomeBean("Value12", "Value22", "Value32"));
-		
-		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field3");
-		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBean Filter", filter);
-		MappingJacksonValue mapping = new MappingJacksonValue(list);
-		mapping.setFilters(filters);
-		return mapping;
-	}
-	
-	
-	
-	private MappingJacksonValue getMapping(SomeBean someBean, String [] consider){
-		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept(consider);
-		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBean Filter", filter);
-		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
-		mapping.setFilters(filters);
-		return mapping;
-	}
-	
+	// Redundant Code
+	// SimpleBeanPropertyFilter filter =
+	// SimpleBeanPropertyFilter.filterOutAllExcept("field1","field3");
+	// FilterProvider filters = new
+	// SimpleFilterProvider().addFilter("SomeBean Filter", filter);
+	// MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+	// mapping.setFilters(filters);
 }
